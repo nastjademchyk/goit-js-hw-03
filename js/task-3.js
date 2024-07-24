a9d849455349024fd96a9896870c4606fab3f594
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-07-24 15:07:18 +0200

    resolved 3 tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,15 @@
-function getElementWidth(content, padding, border) {
-        const contentWidth = parseFloat(content);
-        const paddingWidth = parseFloat(padding);
-        const borderWidth = parseFloat(border);
-  return contentWidth + 2 * paddingWidth + 2 * borderWidth;
+function filterArray(numbers, value) {
+    const newArray = [];
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] > value) {
+            newArray.push(numbers[i]); 
+        }
+    }
+    return newArray;
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4)); 
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20)); 
